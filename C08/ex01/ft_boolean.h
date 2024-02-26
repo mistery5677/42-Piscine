48367dc54185dff5e5c6f255d80978d9f1ab944b
unknown <miguel.costa.5677@gmail.com>
2024-02-26 14:33:03 +0000

    C08 ex00 ex01

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+#include <unistd.h>
+#define EVEN_MSG "I have an even number of arguments.\n"
+#define ODD_MSG "I have an odd number of arguments.\n"
+#define SUCCESS 0
+
+typedef enum { 
+    FALSE = 0,
+    TRUE = 1
+} t_bool;
+//tem a função de definir um tipo, mas com outro nome, podiamos também definir um "int", para "MeuInt" ... por exemplo
+//A expressão "enum", serve para enumerar várias constantes, como o boleano funciona como "true / false" ou "1 / 0", mas o subject pede para definir "TRUE / FALSE", então neste caso temos de definir valores
+
+bool EVEN(int nbr)
+{
+    if(nbr % 2 == 0)
+        return true;
+    else
+        return false;
+}
---
+++ libft/main.c
@@ -0,0 +1,7 @@
+#include "libft.h"
+
+int main()
+{
+    hello();
+    return 0;
+}
