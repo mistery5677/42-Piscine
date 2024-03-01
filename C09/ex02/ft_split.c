1f5365a552b770535702a13284f024ed6aa3d747
Miguel Costa <mistery576@DESKTOP-CSR6V0T>
2024-03-01 16:31:04 +0000

    finito

--- C09/ex02/ft_split.c
+++ C09/ex02/ft_split.c
@@ -1,70 +1,128 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+char *str_start(char *str, char *charset, int i) //Vai passar as palavras, até encontrar uma letra que não esteja no "charset"
+{
+        //int i;
+        int j;
+
+        //i = 0;
+        while(str[i])
+        {
+                j = 0;
+                while((str[i] != charset[j]) && charset[j]) //comparar a str com o "*charset"
+                        j++;
+                if(str[i] == charset[j])
+                        i++;
+                else
+                        return str + i;
+        }
+        return str + i;
+}
+
 int count_words(char *str, char *charset)
 {
-    int i;
-    int j;
-    int words;
+        int i;
+        int j;
+        int words;
 
-    i = 0;
-    words = 0;
-    while(str[i])
-    {
-        j = 0;
-        while(charset[j] != str[i] && charset[j] != '\0')
-            j++;
-        if(charset[j] == str[i])
-            words++;
-        i++;
-    }
-    return words;
+        i = 0;
+        words = 1;
+        str = str_start(str, charset, 0);
+        if(str[0] == '\0')
+                return 0;
+        while(str[i])
+        {
+                j = 0;
+                while((str[i] != charset[j]) && charset[j]) //comparar a str com o "*charset"
+                        j++;
+                //printf("str: %c\n", str[i]);
+                //printf("charset: %c\n", charset[i]);
+                if(str[i] == charset[j])
+                {
+                        //printf("Entrada: %s\n", str);
+                        str = str_start(str + i, charset, 0);
+                        i = 0;
+                        if (str[i] != '\0') //Colocamos esta condição para que não conte como palavra se tivermos 2 charset no final EX"hhh"
+                                words++;
+                }
+                i++;
+        }
+        return words;
 }
 
-int len_word(char *str, char *charset)
+int word_len(char *str, char *charset)
 {
-    int i;
-    int j;
+        int i;
+        int j;
+        int size;
 
-    i = 0;
-    printf("string %s\n", str);
-    while(str[i])
-    {
-        j = 0;
-        while(charset[j] != '\0' && (charset[j] != str[i]))
-            j++;
-        if(charset[j] == str[i])
-            return i;
-        i++;
-    }
-    printf("Terminou %d\n\n", i);
-    return i;
+        size = 0;
+        i = 0;
+        while(str[i])
+        {
+                j = 0;
+                while((str[i] != charset[j]) && charset[j]) //comparar a str com o "*charset"
+                        j++;
+                if(str[i] == charset[j])
+                        return size;
+                size++;
+                i++;
+        }
+        return size;
 }
 
-int ft_split(char *str, char *charset)
+void build_str(char *split, char *str, int size)
 {
-    char **split;
-    int i; //percorrer a "str"
-    //int j; //percorrer o "split"
-    int words;
+        int i;
+        
+        i = 0;
+        while(i < size)
+        {
+                split[i] = str[i];
+                i++;
+        }
+        split[i] = '\0';
+}
 
-    words = count_words(str,charset);
-    i = 0;
-    //split = malloc((count_words(str, charset) + 1) * sizeof(char));
-    printf("Words %d\n\n", count_words(str,charset));
-    while(i <= words)
-    {
-        //split[j] = malloc((len_word(str, charset, i)) *sizeof(char))
-        str = (str + len_word(str, charset)) + 1;
-        i++;
-    }
-    return 1;
+char **ft_split(char *str, char *charset)
+{
+	char **split;
+	int i; //percorrer a "str"
+	int words;
+        int size_word;
+
+        size_word = 0;
+	i = 0;
+	words = count_words(str,charset);
+	split = malloc((words + 1) * sizeof(char*));
+	if(split == NULL)
+		return NULL;
+	while(i < words)
+	{
+                str = str_start(str, charset, size_word); //Para podermos percorrer a "str" toda, temos de adicionar "size_word", em que vai passar essa informação para a função "str_start"
+                size_word = word_len(str, charset);
+		split[i] = malloc((size_word + 1) *sizeof(char));
+		if(split[i] == NULL)
+			return NULL;
+		build_str(split[i], str, size_word);
+		i++;
+	}
+	return split;
 }
 
 int main()
 {
-    char str[15] = "";
-    char *charset = "04";
+	char str[20] = "";
+	char *charset = "h04";
+	char **split = ft_split(str, charset);
 
-    printf("final %d\n", ft_split(str,charset));
+        //printf("%s\n", str_start(str, charset)); //TESTAR STR_START
+        //printf("%d\n\n", count_words(str, charset)); //TESTAR COUNT_WORDS
+        //printf("string: %s\n", str);
+        //printf("word_len: %d\n", word_len(str, charset)); //TESTAR O TAMANHO DA STRING
+
+        
+
+	//free(split);
 }
