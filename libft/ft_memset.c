0c3e6c260e5b8291a92e2ea9a73bbf6a69f7df52
Miguel Costa <miguelcosta@Air-de-Miguel.lan>
2024-03-02 21:53:03 +0000

    updated

---
+++ libft/ft_memset.c
@@ -0,0 +1,3 @@
+#include "libft.h"
+
+void *ft_memset(void s[n], int c, size_t n)
--- libft/main.c
+++ libft/main.c
@@ -4,20 +4,30 @@
 
 int main()
 {
-    char teste = '+';
+    char c = '\0';
+    char str[10] = "hello";
+
     printf("TESTING FT_ISALPHA \n\n");
-    printf("my: %d\n", ft_isalpha(teste));
-    printf("original: %d\n\n", isalpha(teste));
+    printf("my: %d\n", ft_isalpha(c));
+    printf("original: %d\n\n", isalpha(c));
 
     printf("TESTING FT_ISDIGIT \n\n");
-    printf("my: %d\n", ft_isdigit(teste));
-    printf("original: %d\n\n", isdigit(teste));
+    printf("my: %d\n", ft_isdigit(c));
+    printf("original: %d\n\n", isdigit(c));
     
     printf("TESTING FT_ISALNUM \n\n");
-    printf("my: %d\n", ft_isalnum(teste));
-    printf("original: %d\n\n", isalnum(teste));
+    printf("my: %d\n", ft_isalnum(c));
+    printf("original: %d\n\n", isalnum(c));
 
     printf("TESTING FT_ISASCII \n\n");
-    printf("my: %d\n", ft_isascii(teste));
-    printf("original: %d\n\n", isascii(teste));
+    printf("my: %d\n", ft_isascii(c));
+    printf("original: %d\n\n", isascii(c));
+
+    printf("TESTING FT_ISPRINT \n\n");
+    printf("my: %d\n", ft_isprint(c));
+    printf("original: %d\n\n", isprint(c));
+
+    printf("TESTING FT_STRLEN \n\n");
+    printf("my: %lu\n", ft_strlen(str));
+    printf("original: %lu\n\n", strlen(str));
 }
