b62f57c78580b0a70361961165539cdb0ddf6bb4
Miguel Costa <miguelcosta@Air-de-Miguel.lan>
2024-03-08 11:30:16 +0000

    str_join updated

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -2,22 +2,34 @@
 
 char *ft_strjoin(char const *s1, char const *s2)
 {
-        int i;
-        int len_s1;
-        int len_s2;
+        unsigned int i;
+        size_t len_s1;
+        size_t len_s2;
         char *new_str;
 
-        i = 0;
+        i = -1;
         len_s1 = ft_strlen(s1);
         len_s2 = ft_strlen(s2);
-        new_str = malloc(((len_s1 + len_s2) + 1) + sizeof(char));
-        if(new_str == NULL || len_s1 == 0 || len_s2 == 0)
+        if(len_s1 == 0 && len_s2 == 0)
                 return NULL;
-        while(s1[i])
-        {
+        new_str = malloc(((len_s1 + len_s2) + 1) + sizeof(char));
+        if(new_str == NULL)
+                return NULL;
+        while(s1[++i])
                 new_str[i] = s1[i];
-                i++;
-        }
-        ft_strlcat(new_str, s2, len_s1 + len_s2);
+        i = -1;
+        while(s2[++i])
+                new_str[len_s1++] = s2[i];
+        new_str[len_s1] = '\0';       
         return new_str;
+}
+
+int main()
+{
+    const char s1_strjoin[] = "hello";
+    const char s2_strjoin[] = "";
+    char *result_strjoin = ft_strjoin(s1_strjoin, s2_strjoin);
+    printf("TESTING FT_STRJOIN \n\n");
+    printf("meu: %s\n\n", result_strjoin);
+    free(result_strjoin);
 }
