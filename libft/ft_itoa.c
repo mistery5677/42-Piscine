510cf407c701e82d6f8a83b9db732664e473433b
Miguel Costa <mistery576@DESKTOP-CSR6V0T>
2024-03-10 00:52:26 +0000

    updated 10/03

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -3,18 +3,20 @@
 size_t divide_nbr(int n)
 {
     int i;
+    long int nbr;
 
+    nbr = n;
     i = 0;
-    if(n < 0)
+    if(nbr < 0)
     {
-        n *= -1;
+        nbr *= -1;
         i++;
     }
-    if(n == 0)
+    if(nbr == 0)
         return 1;
-    while(n > 0)
+    while(nbr > 0)
     {
-        n = n / 10;
+        nbr = nbr / 10;
         i++;
     }
     return i;
@@ -22,6 +24,7 @@ size_t divide_nbr(int n)
 
 void build_p(char *str, int n, int size)
 {
+    str[size] = '\0';
     while(--size >= 0)
     {
         str[size] = (n % 10) + '0';;
@@ -31,11 +34,15 @@ void build_p(char *str, int n, int size)
 
 void build_n(char *str, int n, int size)
 {
-    n *= -1;
+    long int nbr;
+
+    nbr = n;
+    nbr *= -1;
+    str[size] = '\0';
     while(--size > 0)
     {
-        str[size] = (n % 10) + '0';;
-        n = n /10;  
+        str[size] = (nbr % 10) + '0';;
+        nbr = nbr /10;  
     }
     str[size] = '-';
 }
@@ -43,22 +50,23 @@ void build_n(char *str, int n, int size)
 char *ft_itoa(int n)
 {
     char *str;
-    long nbr = n;
     size_t size;
 
-    size = divide_nbr(nbr);
+    size = divide_nbr(n);
     str =  malloc((size + 1) * sizeof(char));
+    if(!str)
+        return NULL;
     if (n >= 0)
-        build_p(str, nbr, size);
+        build_p(str, n, size);
     else
-        build_n(str, nbr, size);
+        build_n(str, n, size);
     return str;
 }
 
 /*int main()
 {
-    printf("123 --> %s\n", ft_itoa(122231));
-    printf("0 --> %s\n", ft_itoa(0));
-    printf("-123 --> %s\n", ft_itoa(-12321312));
+    printf("123 --> %s\n", ft_itoa(-2147483648));
+    //printf("0 --> %s\n", ft_itoa(0));
+    //printf("-123 --> %s\n", ft_itoa(-12321312));
     //printf("divided by %d\n", divide_nbr(n_itoa));
 }*/
--- libft/libft.h
+++ libft/libft.h
@@ -6,6 +6,12 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+typedef struct s_list
+{
+        void *content;
+        struct s_list *next;
+} t_list;
+
 int ft_isalpha(int c);
 int ft_isdigit(int c);
 int ft_isalnum(int c);
@@ -39,4 +45,9 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void ft_striteri(char *s, void (*f)(unsigned int, char *));
 void ft_putchar_fd(char c, int fd);
 void ft_putstr_fd(char *c, int fd);
+t_list *ft_lstnew(void *content);
+void ft_lstadd_front(t_list **lst, t_list *new);
+int ft_lstsize(t_list *lst);
+t_list *ft_lstlast(t_list *lst);
+
 #endif
